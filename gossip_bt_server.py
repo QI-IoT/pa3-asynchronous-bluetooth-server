18a682730c1c21acf9e0ebd49b31b8a1d2c29977
Ching-han Yu <chinghanyu@ntu.edu.tw>
2017-07-27 10:10:05 -0700

    Add arg parser and json output format

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -1,12 +1,21 @@
-import asyncore
-
 from btserver import BTServer
 from bterror import BTError
+
+import argparse
+import asyncore
+import json
 from random import uniform
 from threading import Thread
 from time import sleep, time
 
 if __name__ == '__main__':
+    # Create option parser
+    usage = "usage: %prog [options] arg"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", dest="output_format", default="csv", help="set output format: csv, json")
+
+    args = parser.parse_args()
+
     # Create a BT server
     uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
     service_name = "GossipBTServer"
@@ -21,17 +30,28 @@ if __name__ == '__main__':
         for client_handler in server.active_client_handlers.copy():
             # Use a copy() to get the copy of the set, avoiding 'set change size during iteration' error
             # Create CSV message "'realtime', time, temp, SN1, SN2, SN3, SN4, PM25\n"
-            msg = "realtime, %d, %f, %f, %f, %f, %f, %f\n" % \
-                  (int(time()),         # epoch time
-                   uniform(20, 30),     # random temperature
-                   uniform(40, 50),     # random SN1 value
-                   uniform(60, 70),     # random SN2 value
-                   uniform(80, 90),     # random SN3 value
-                   uniform(100, 110),   # random SN4 value
-                   uniform(120, 130))   # random PM25 value
+            epoch_time = int(time())    # epoch time
+            temp = uniform(20, 30)      # random temperature
+            SN1 = uniform(40, 50)       # random SN1 value
+            SN2 = uniform(60, 70)       # random SN2 value
+            SN3 = uniform(80, 90)       # random SN3 value
+            SN4 = uniform(100, 110)     # random SN4 value
+            PM25 = uniform(120, 130)    # random PM25 value
 
+            msg = ""
+            if args.output_format == "csv":
+                msg = "realtime, %d, %f, %f, %f, %f, %f, %f" % (epoch_time, temp, SN1, SN2, SN3, SN4, PM25)
+            elif args.output_format == "json":
+                output = {'type': 'realtime',
+                          'time': epoch_time,
+                          'SN1': SN1,
+                          'SN2': SN2,
+                          'SN3': SN3,
+                          'SN4': SN4,
+                          'PM25': PM25}
+                msg = json.dumps(output)
             try:
-                client_handler.send(msg)
+                client_handler.send(msg + '\n')
             except Exception as e:
                 BTError.print_error(handler=client_handler, error=BTError.ERR_WRITE, error_message=repr(e))
                 client_handler.handle_close()
